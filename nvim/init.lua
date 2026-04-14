194a65490ba0aa488691ae41c35c20bc9fa8f2af
dntfury <37257695+dntfury@users.noreply.github.com>
2026-04-14 17:35:53 +0200

    feat: add vim wiki - instuction on setting vim wiki - remove lazy vim plugin setup - make init.lua simpler

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,50 +14,30 @@ vim.opt.ignorecase = true          -- Makes search case-insensitive
 vim.opt.smartcase = true           -- Overrides ignorecase
 vim.opt.incsearch = true           -- Shows search matches as you type
 
-
--- Plugins (using lazy.nvim)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- This builds a path like ~/.local/share/nvim/lazy/lazy.nvim
-
--- Step 2: Check if lazy.nvim is already installed at lazypath, if not installed it will install it at
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath
-    })
-end
-
--- Step 3: Prepend lazy.nvim's path to Neovim's runtime path
-vim.opt.rtp:prepend(lazypath)
-
-
--- Plugin & LSP setup ( MINIMUM )
-require("lazy").setup({
-    "williamboman/mason.nvim",          -- Plugin that manages external tools (LSP servers, linters, formatters)
-    "williamboman/mason-lspconfig.nvim", -- Bridge between mason.nvim and lspconfig for easy LSP installation
-    "neovim/nvim-lspconfig",            -- Neovim's built-in LSP client configuration plugin
+-- Open all help in a new tab by default
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  callback = function()
+    vim.cmd("wincmd T")
+  end
 })
 
--- Initialize mason.nvim
-require("mason").setup()
+-- vim wiki config start
+---- pluigin setup start
+-- to get vim wiki plugin
+-- mkdir -p ~/.config/nvim/pack/plugins/start
+-- git clone https://github.com/vimwiki/vimwiki.git ~/.config/nvim/pack/plugins/start/vimwiki
+---- vim wiki pluing setup done
+vim.g.vimwiki_list = {
+  {
+    path = vim.fn.expand("~/vimwiki/"),
+    syntax = "markdown",
+    ext = ".md",
 
--- Configure mason-lspconfig to automatically ensure these LSP servers are installed
-require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "gopls"},    -- Automatically install and keep lua_ls language server up to date
-})
-
-local lspconfig = require("lspconfig")
-
--- set up language server with default settings (after mason ensures it is installed)
-lspconfig.lua_ls.setup({})
-lspconfig.gopls.setup({})
-
--- Keymaps for LSP
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
--- 'gd' in normal mode: go to the definition of the symbol under the cursor
--- Useful for jumping to the function/variable declaration
-
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- 'K' in normal mode: show hover information (e.g. function signature, documentation)
--- Like tooltip info when you hover with a mouse in an IDE
+    diary_rel_path = "diary/",
+    diary_index = "diary",
+    diary_header = "Diary",
+  },
+}
+vim.g.vimwiki_global_ext = 0
+-- vim wiki config end
